c6ba977cb099e5e2e4ef1b9c6806ad0874e21c93
Afikur Rahman <sudipto.ruet@gmail.com>
2017-05-07 18:24:07 +0600

    cart added

---
+++ app/src/main/java/gnt/com/simple_database_connection/LocationHandler.java
@@ -0,0 +1,8 @@
+package gnt.com.simple_database_connection;
+
+/**
+ * Created by PC-05 on 5/7/2017.
+ */
+
+public class LocationHandler {
+}
---
+++ app/src/main/java/gnt/com/simple_database_connection/ShowCart.java
@@ -0,0 +1,13 @@
+package gnt.com.simple_database_connection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowCart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_cart);
+    }
+}
